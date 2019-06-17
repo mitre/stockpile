389fdec5a06ce7f78a7c3afe7f2ee67f5c6726f2
david <davidhunt@mitre.org>
2019-06-17 11:11:24 -0500

    updating disabled

--- payloads/wifi.ps1
+++ payloads/wifi.ps1
@@ -13,7 +13,7 @@ if ($scan){
    netsh interface set interface name="Wi-Fi" admin=ENABLED
 } elseif ($off) {
    Write-Host "Turning WIFI off"
-   netsh interface set interface name="Wi-Fi" admin=ENABLED
+   netsh interface set interface name="Wi-Fi" admin=DISABLED
 } elseif ($pref) {
    Write-Host "Getting preferred WIFI networks"
    netsh wlan show profiles
