a2286e59d8d64afa70180a16657f1d337746f30e
Hunt, David <davidhunt@mitre.org>
2019-10-16 09:34:48 -0400

    changes

--- data/parsers/p_email.py
+++ data/parsers/p_email.py
@@ -3,6 +3,10 @@ from plugins.stockpile.data.parsers.p_base import BaseParser, Relationship
 
 class EmailParser(BaseParser):
 
+    def __init__(self, variables):
+        self.user = variables.get('host.user.name')
+        self.org = variables.get('target.org.name')
+
     def parse(self, blob):
         """
         Parse out email addresses
