f9bd06af20945f6d40d4d4bd39b3e436197a789b
clittle <clittle@mitre.org>
2019-10-24 15:03:49 -0400

    Working and tested.
    The parser is exhaustive and could be made simpler/prioritized to focus
    on less things. However, I feel like the ability to parse the additional
    information out of Mimikatz's output will prove useful in the future, so
    I intend to leave it in, as although expansive, the additional parsing
    work is straightforward.

--- app/parsers/katz.py
+++ app/parsers/katz.py
@@ -1,25 +1,27 @@
 from plugins.stockpile.app.parsers.base_parser import BaseParser
 from plugins.stockpile.app.relationship import Relationship
+from app.utility.logger import Logger
 
 import re
 from collections import defaultdict
 
 class MimikatzBlock(object):
     def __init__(self):
-        self.session = ""
-        self.username = ""
-        self.domain = ""
-        self.logon_server = ""
-        self.logon_time = ""
-        self.sid = ""
+        self.session = ''
+        self.username = ''
+        self.domain = ''
+        self.logon_server = ''
+        self.logon_time = ''
+        self.sid = ''
         self.packages = defaultdict(list)
 
 
-class Katz(BaseParser):
+class Parser(BaseParser):
     def __init__(self, parser_info):
         self.mappers = parser_info['mappers']
         self.used_facts = parser_info['used_facts']
-        self.parse_base = 'wdigest'
+        self.parse_mode = 'wdigest'
+        self.log = Logger('parsing_svc')
 
     def parse_kats(self, output):
         """
@@ -30,12 +32,12 @@ class Katz(BaseParser):
             A list of MimikatzSection objects
         """
         # split sections using "Authentication Id" as separator
-        sections = output.split("Authentication Id")
+        sections = output.split('Authentication Id')
         creds = []
-        for section in sections[1:]:
+        for section in sections:
             mk_section = MimikatzBlock()
             package = {}
-            package_name = ""
+            package_name = ''
             in_header = True
             for line in section.splitlines():
                 line = line.strip()
@@ -43,22 +45,22 @@ class Katz(BaseParser):
                     if line.startswith('msv'):
                         in_header = False
                     else:
-                        session = re.match(r"^\s*Session\s*:\s*([^\r\n]*)", line)
+                        session = re.match(r'^\s*Session\s*:\s*([^\r\n]*)', line)
                         if session:
                             mk_section.session = session.group(1)
-                        username = re.match(r"^\s*User Name\s*:\s*([^\r\n]*)", line)
+                        username = re.match(r'^\s*User Name\s*:\s*([^\r\n]*)', line)
                         if username:
                             mk_section.username = username.group(1)
-                        domain = re.match(r"^\s*Domain\s*:\s*([^\r\n]*)", line)
+                        domain = re.match(r'^\s*Domain\s*:\s*([^\r\n]*)', line)
                         if domain:
                             mk_section.domain = domain.group(1)
-                        logon_server = re.match(r"^\s*Logon Server\s*:\s*([^\r\n]*)", line)
+                        logon_server = re.match(r'^\s*Logon Server\s*:\s*([^\r\n]*)', line)
                         if logon_server:
                             mk_section.logon_server = logon_server.group(1)
-                        logon_time = re.match(r"^\s*Logon Time\s*:\s*([^\r\n]*)", line)
+                        logon_time = re.match(r'^\s*Logon Time\s*:\s*([^\r\n]*)', line)
                         if logon_time:
                             mk_section.logon_time = logon_time.group(1)
-                        sid = re.match(r"^\s*SID\s*:\s*([^\r\n]*)", line)
+                        sid = re.match(r'^\s*SID\s*:\s*([^\r\n]*)', line)
                         if sid:
                             mk_section.sid = sid.group(1)
                         continue
@@ -73,13 +75,13 @@ class Katz(BaseParser):
                     package = {}
                     pass
                 elif line.startswith('*'):
-                    m = re.match(r"\s*\* (.*?)\s*: (.*)", line)
+                    m = re.match(r'\s*\* (.*?)\s*: (.*)', line)
                     if m:
                         package[m.group(1)] = m.group(2)
 
                 elif line:
                     # parse out the new section name
-                    match_group = re.match(r"([a-z]+) :", line)
+                    match_group = re.match(r'([a-z]+) :', line)
                     if match_group:
                         # this is the start of a new ssp
                         # save the current ssp if necessary
@@ -106,14 +108,17 @@ class Katz(BaseParser):
 
     def parse(self, blob):
         relationships = []
-        parse_data = self.parse_kats(blob)
-        for match in parse_data:
-            if self.parse_mode in match.packages:
-                for mp in self.mappers:
-                    relationships.append(
-                        Relationship(source=(mp.get('source'), match.package[self.parse_mode]['Username']),
-                                     edge=mp.get('edge'),
-                                     target=(mp.get('target'), match.package[self.parse_mode]['Password']))
-                    )
+        try:
+            parse_data = self.parse_kats(blob)
+            for match in parse_data:
+                if self.parse_mode in match.packages:
+                    for mp in self.mappers:
+                        relationships.append(
+                            Relationship(source=(mp.get('source'), match.packages[self.parse_mode][0]['Username']),
+                                         edge=mp.get('edge'),
+                                         target=(mp.get('target'), match.packages[self.parse_mode][0]['Password']))
+                        )
+        except Exception as error:
+            self.log.warning('Mimikatz parser encountered an error - {}. Continuing...'.format(error))
         return relationships
 
