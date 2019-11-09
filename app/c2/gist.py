3101d2b6432302193a6ddce401ac94fdc9cd5ea9
brianedmonds90 <bedmonds@mitre.org>
2019-11-08 21:44:04 -0500

    initial functionality

---
+++ app/c2/gist.py
@@ -0,0 +1,26 @@
+import json
+from app.objects.c_c2 import C2
+from github import Github
+
+
+class Gist(C2):
+
+    def __init__(self, module_info):
+        self.key = module_info['config']['keys'][0]
+        self.c2_type = module_info['c2_type']
+
+    def encode_config_info(self):
+        """ Returns one of the API keys to be encoded into the agent """
+        return self.key
+
+    def recieve_beacon(self):
+        """ """
+        pass
+
+    def handle_beacon(self):
+        g = Github(self.key)
+        for gist in g.get_user().get_gists():
+            if 'beacon' == gist.description:
+                for file in gist.files:
+                    print(gist.files[file].content)
+                    #data = json.loads(gist.files[file].content)
