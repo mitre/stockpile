08e64d9e89f41ec8477a1324725318061bf2ed59
brianedmonds90 <bedmonds@mitre.org>
2019-11-09 07:47:52 -0500

    choosing a random github key for the c2

--- app/c2/gist.py
+++ app/c2/gist.py
@@ -1,4 +1,5 @@
-import json
+import random
+
 from app.objects.c_c2 import C2
 from github import Github
 
@@ -6,7 +7,8 @@ from github import Github
 class Gist(C2):
 
     def __init__(self, module_info):
-        self.key = module_info['config']['keys'][0]
+        index = random.randint(0, len(module_info['config']['keys'])-1)
+        self.key = module_info['config']['keys'][index]
         self.c2_type = module_info['c2_type']
 
     def encode_config_info(self):
