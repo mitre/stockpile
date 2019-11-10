9fd47b34ecf3c9245c3099ead56faa78a4de08e0
brianedmonds90 <bedmonds@mitre.org>
2019-11-10 10:20:46 -0500

    moving aiohttp parsing of github because we must

--- app/c2/gist.py
+++ app/c2/gist.py
@@ -1,9 +1,10 @@
 import random
 import json
+import uuid
+import pprint
+import aiohttp
 
-from base64 import b64encode, b64decode
 from app.objects.c_c2 import C2
-from github import Github
 
 
 class Gist(C2):
@@ -18,18 +19,38 @@ class Gist(C2):
         return self.key
 
     async def get_beacons(self):
-        """ Retrieve all GitHub gists for a particular api key"""
-        g = Github(self.key)
-        beacons = []
-        for gist in g.get_user().get_gists():
-            if 'beacon' == gist.description:
-                for file in gist.files:
-                    beacon = self._decode_bytes(gist.files[file].content)
-                    beacons.append(json.loads(beacon))
+        """ Retrieve all GitHub gist communication for a particular api key"""
+        headers = {'Authorization': 'token {}'.format(self.key)}
+        beacons, beacon_urls = [], []
+        async with aiohttp.ClientSession(headers=headers, connector=aiohttp.TCPConnector(verify_ssl=False)) as session:
+            gists = json.loads(await self._fetch(session, 'https://api.github.com/gists'))
+            for gist in gists:
+                if 'beacon' in gist['description']:
+                    for file in gist.get('files'):
+                        raw_url = gist['files'][file]['raw_url']
+                        beacon_urls.append(raw_url)
+
+        for url in beacon_urls:
+            async with aiohttp.ClientSession(headers=headers, connector=aiohttp.TCPConnector(verify_ssl=False)) as session:
+                beacon = json.loads(self.decode_bytes(await self._fetch(session, url)))
+                beacons.append(beacon)
+
         return beacons
+
+
+
+
+
+    async def post_instructions(self, instructions, paw):
+        """ Post an instruction for the agent to execute as an encoded GIST """
+        
+        return None
+
 
     """ PRIVATE """
 
     @staticmethod
-    def _decode_bytes(s):
-        return b64decode(s).decode('utf-8', errors='ignore').replace('\n', '')
+    async def _fetch(session, url):
+        async with session.get(url) as response:
+            return await response.text()
+
