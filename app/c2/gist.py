705f3fa01b316cc1bff0c67c639f90a8d5f44675
brianedmonds90 <bedmonds@mitre.org>
2019-11-10 15:08:13 -0500

    should be final commit to gist c2

--- app/c2/gist.py
+++ app/c2/gist.py
@@ -1,7 +1,6 @@
 import random
 import json
 import uuid
-import pprint
 import aiohttp
 
 from app.objects.c_c2 import C2
@@ -20,34 +19,52 @@ class Gist(C2):
 
     async def get_results(self):
         """ Retrieve all GIST posted results for a particular api key"""
-        result_urls = await self._get_raw_gist_urls(comm_type='results')
-        return await self._get_gist_content(result_urls)
+        results = await self._get_raw_gist_urls(comm_type='results')
+        result_content = await self._get_gist_content([result[0] for result in results])
+        await self._delete_gists([result[1] for result in results])
+        return result_content
 
     async def get_beacons(self):
         """ Retrieve all GIST beacons for a particular api key"""
-        beacon_urls = await self._get_raw_gist_urls(comm_type='beacon')
-        return await self._get_gist_content(beacon_urls)
+        beacons = await self._get_raw_gist_urls(comm_type='beacon')
+        beacon_content = await self._get_gist_content([beacon[0] for beacon in beacons])
+        await self._delete_gists([beacon[1] for beacon in beacons])
+        return beacon_content
+
+    async def post_payloads(self, payloads, paw):
+        """
+
+        :param payloads:
+        :param paw:
+        :return:
+        """
+        files = {payload[0]: dict(content=self.encode_string(str(payload[1]))) for payload in payloads}
+        if len(files) < 1 or await self._wait_for_paw(paw, comm_type='payloads'):
+            return
+        gist = self._build_gist_content(comm_type='payloads', paw=paw, files=files)
+        return await self._post_gist(gist)
 
     async def post_instructions(self, text, paw):
         """ Post an instruction for the agent to execute as an encoded GIST """
-        if len(json.loads(self.decode_bytes(text))['instructions']) < 1:
+        if len(json.loads(self.decode_bytes(text))['instructions']) < 1 or \
+                await self._wait_for_paw(paw, comm_type='instructions'):
             return
-        if await self._paw_has_instructions(paw):
-            return
+        gist = self._build_gist_content(comm_type='instructions', paw=paw, files={str(uuid.uuid4()): {"content": text}})
+        return await self._post_gist(gist)
+
+    """ PRIVATE """
+
+    async def _post_gist(self, gist):
         headers = {'Authorization': 'token {}'.format(self.key)}
-        gist = {
-            "description": "instructions-{}".format(paw),
-            "public": False,
-            "files": {
-                str(uuid.uuid4()): {
-                    "content": text
-                }
-            }
-        }
         async with aiohttp.ClientSession(headers=headers, connector=aiohttp.TCPConnector(verify_ssl=False)) as session:
             return await self._post(session, 'https://api.github.com/gists', body=gist)
 
-    """ PRIVATE """
+    @staticmethod
+    def _build_gist_content(comm_type, paw, files):
+        return {
+            "description": "{}-{}".format(comm_type, paw),
+            "public": False,
+            "files": files}
 
     async def _get_gist_content(self, urls):
         all_content = []
@@ -64,13 +81,19 @@ class Gist(C2):
             if comm_type in gist['description']:
                 for file in gist.get('files'):
                     raw_url = gist['files'][file]['raw_url']
-                    raw_urls.append(raw_url)
+                    raw_urls.append((raw_url, gist['id']))
         return raw_urls
 
     async def _get_gists(self):
         headers = {'Authorization': 'token {}'.format(self.key)}
         async with aiohttp.ClientSession(headers=headers, connector=aiohttp.TCPConnector(verify_ssl=False)) as session:
             return json.loads(await self._fetch(session, 'https://api.github.com/gists'))
+
+    async def _delete_gists(self, gist_ids):
+        headers = {'Authorization': 'token {}'.format(self.key)}
+        for _id in gist_ids:
+            async with aiohttp.ClientSession(headers=headers, connector=aiohttp.TCPConnector(verify_ssl=False)) as session:
+                await self._delete(session, 'https://api.github.com/gists/{}'.format(_id))
 
     @staticmethod
     async def _fetch(session, url):
@@ -82,8 +105,13 @@ class Gist(C2):
         async with session.post(url, json=body) as response:
             return await response.text()
 
-    async def _paw_has_instructions(self, paw):
+    async def _wait_for_paw(self, paw, comm_type):
         for gist in await self._get_gists():
-            if 'instructions-{}'.format(paw) == gist['description']:
+            if '{}-{}'.format(comm_type, paw) == gist['description']:
                 return True
         return False
+
+    @staticmethod
+    async def _delete(session, url):
+        async with session.delete(url) as response:
+            return await response.text()
