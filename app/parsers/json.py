355c20392a3ff31f090c07a35671cac86fa4e991
garunagiri <garunagiri@mitre.org>
2019-11-12 13:25:26 -0500

    fixed function calls

--- app/parsers/json.py
+++ app/parsers/json.py
@@ -14,14 +14,13 @@ class Parser(BaseParser):
 
     def parse(self, blob):
         relationships = []
-        json_obj = self._load_json(blob)
-        if json_obj is not None:
+        json_output = self._load_json(blob)
+        if json_output is not None:
             for mp in self.mappers:
-                json_key = mp.misc.get('json_key')
-                if json_key is None:
+                if "json_key" not in dir(mp):
                     self.log.warning("JSON Parser not given a 'json_key', not parsing")
                     continue
-                for match in self._get_vals_from_json(json_obj, json_key):
+                for match in self._get_vals_from_json(json_output, mp.json_key):
                     source = self.set_value(mp.source, match, self.used_facts)
                     target = self.set_value(mp.target, match, self.used_facts)
                     relationships.append(
@@ -38,14 +37,14 @@ class Parser(BaseParser):
             self.log.warning("Output not JSON, use a different parser")
             return None
 
-    def _get_vals_from_json(self, json_obj, key):
+    def _get_vals_from_json(self, json_output, key):
         """Get all values for a specified key recursively from JSON output."""
-        if isinstance(json_obj, list):
-            for item in json_obj:
+        if isinstance(json_output, list):
+            for item in json_output:
                 for res in self._get_vals_from_json(item, key):
                     yield res
-        elif isinstance(json_obj, dict):
-            for k, v in json_obj.items():
+        elif isinstance(json_output, dict):
+            for k, v in json_output.items():
                 if k == key:
                     yield json.dumps(v)
                 if isinstance(v, list) or isinstance(v, dict):
