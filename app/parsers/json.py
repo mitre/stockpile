93e5027449379bf12f55eaa111cb7c267c4dd5bc
garunagiri <garunagiri@mitre.org>
2019-11-12 11:38:43 -0500

    added logger

--- app/parsers/json.py
+++ app/parsers/json.py
@@ -2,20 +2,26 @@ import json
 
 from app.objects.c_relationship import Relationship
 from plugins.stockpile.app.parsers.base_parser import BaseParser
+from app.utility.logger import Logger
 
 
 class Parser(BaseParser):
 
+    def __init__(self, parser_info):
+        self.mappers = parser_info['mappers']
+        self.used_facts = parser_info['used_facts']
+        self.log = Logger('parsing_svc')
+
     def parse(self, blob):
         relationships = []
-        json_obj = self.__load_json(blob)
+        json_obj = self._load_json(blob)
         if json_obj is not None:
             for mp in self.mappers:
                 json_key = mp.misc.get('json_key')
                 if json_key is None:
                     self.log.warning("JSON Parser not given a 'json_key', not parsing")
                     continue
-                for match in self.__get_vals_from_json(json_obj, json_key):
+                for match in self._get_vals_from_json(json_obj, json_key):
                     source = self.set_value(mp.source, match, self.used_facts)
                     target = self.set_value(mp.target, match, self.used_facts)
                     relationships.append(
@@ -36,12 +42,12 @@ class Parser(BaseParser):
         """Get all values for a specified key recursively from JSON output."""
         if isinstance(json_obj, list):
             for item in json_obj:
-                for res in self.__get_vals_from_json(item, key):
+                for res in self._get_vals_from_json(item, key):
                     yield res
         elif isinstance(json_obj, dict):
             for k, v in json_obj.items():
                 if k == key:
                     yield json.dumps(v)
                 if isinstance(v, list) or isinstance(v, dict):
-                    for res in self.__get_vals_from_json(v, key):
+                    for res in self._get_vals_from_json(v, key):
                         yield res
