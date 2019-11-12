28282e94763cffc36a32cf79a599456b6c5b577e
garunagiri <garunagiri@mitre.org>
2019-11-12 09:22:07 -0500

    Comments hopefully addressed properly

--- app/parsers/json.py
+++ app/parsers/json.py
@@ -1,41 +1,46 @@
-from app.objects.c_relationship import Relationship
-from plugins.stockpile.app.parsers.base_parser import BaseParser
 import json
 
+from app.objects.c_relationship import Relationship
+from plugins.stockpile.app.parsers.base_parser import BaseParser
 
 class Parser(BaseParser):
 
     def parse(self, blob):
         relationships = []
-        try:
-            json_obj = json.loads(blob)
-        except Exception as error:
-            self.log.warning("Output not JSON, use a different parser")
-            return []
-        for mp in self.mappers:
-            json_key = mp.misc.get('json_key')
-            if json_key is None:
-                self.log.warning("JSON Parser not given a 'json_key', not parsing")
-                continue
-            for match in self.get_vals_from_json(json_obj, json_key):
-                source = self.set_value(mp.source, match, self.used_facts)
-                target = self.set_value(mp.target, match, self.used_facts)
-                relationships.append(
-                    Relationship(source=(mp.source, source),
-                                 edge=mp.edge,
-                                 target=(mp.target, target))
-                )
+        json_obj = self.__load_json(blob)
+        if json_obj is not None:
+            for mp in self.mappers:
+                json_key = mp.misc.get('json_key')
+                if json_key is None:
+                    self.log.warning("JSON Parser not given a 'json_key', not parsing")
+                    continue
+                for match in self.__get_vals_from_json(json_obj, json_key):
+                    source = self.set_value(mp.source, match, self.used_facts)
+                    target = self.set_value(mp.target, match, self.used_facts)
+                    relationships.append(
+                        Relationship(source=(mp.source, source),
+                                     edge=mp.edge,
+                                     target=(mp.target, target))
+                    )
         return relationships
 
-    def get_vals_from_json(self, json_obj, key):
+    def __load_json(self, blob):
+        try:
+            return json.loads(blob)
+        except Exception as error:
+            self.log.warning("Output not JSON, use a different parser")
+            return None
+
+    def __get_vals_from_json(self, json_obj, key):
+        """Get all values for a specified key recursively from JSON output."""
         if isinstance(json_obj, list):
             for item in json_obj:
-                for res in self.get_vals_from_json(item, key):
+                for res in self.__get_vals_from_json(item, key):
                     yield res
         elif isinstance(json_obj, dict):
             for k, v in json_obj.items():
                 if k == key:
                     yield json.dumps(v)
                 if isinstance(v, list) or isinstance(v, dict):
-                    for res in self.get_vals_from_json(v, key):
+                    for res in self.__get_vals_from_json(v, key):
                         yield res
