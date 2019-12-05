76a982c6e23ce28a8e04b2962e0995b384aab803
Brian Edmonds <bedmonds@mitre.org>
2019-12-05 14:29:20 -0500

    WIP of stopping condition

--- app/sequential.py
+++ app/sequential.py
@@ -1,9 +1,12 @@
 class LogicalPlanner:
 
-    def __init__(self, operation, planning_svc):
+    def __init__(self, operation, planning_svc, stopping_conditions=None):
         self.operation = operation
         self.planning_svc = planning_svc
+        self.stopping_conditions = stopping_conditions
 
     async def execute(self, phase):
         for link in await self.planning_svc.get_links(operation=self.operation, phase=phase):
+            if await self.planning_svc.check_stopping_conditions(self.operation, self.stopping_conditions):
+                return
             await self.operation.apply(link)
