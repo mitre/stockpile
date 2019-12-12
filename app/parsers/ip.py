9c67504da375c6491ac0a3a997896f3b701eb050
Robert Haverkos <rhaverkos@mitre.org>
2019-12-12 17:16:33 -0500

    Improved IP parser and added filtering to arp -a ability

--- app/parsers/ip.py
+++ app/parsers/ip.py
@@ -1,18 +1,62 @@
-from plugins.stockpile.app.parsers.base_parser import BaseParser
 from app.objects.c_relationship import Relationship
+from plugins.stockpile.app.parsers.base_parser import BaseParser
+from ipaddress import IPv4Address as ip_address
+
 
 
 class Parser(BaseParser):
 
     def parse(self, blob):
         relationships = []
-        for match in self.ip(blob):
-            for mp in self.mappers:
-                source = self.set_value(mp.source, match, self.used_facts)
-                target = self.set_value(mp.target, match, self.used_facts)
-                relationships.append(
-                    Relationship(source=(mp.source, source),
-                                 edge=mp.edge,
-                                 target=(mp.target, target))
-                )
+        for ip in self.ip(blob):
+            ip_is_valid = self._is_valid_ip(ip)
+            if ip_is_valid:
+                for mp in self.mappers:
+                    if 'whitelist' in dir(mp):
+                        ip = self._whitelist_ip(ip, mp.whitelist)
+                    if ip:
+                        source = self.set_value(mp.source, ip, self.used_facts)
+                        target = self.set_value(mp.target, ip, self.used_facts)
+                        relationships.append(
+                            Relationship(source=(mp.source, source),
+                                         edge=mp.edge,
+                                         target=(mp.target, target))
+                        )
         return relationships
+
+    @staticmethod
+    def _is_valid_ip(raw_ip):
+        try: 
+            ip = ip_address(raw_ip)
+        except:
+            return False
+        return True
+
+    @staticmethod
+    def _whitelist_ip(raw_ip, whitelist):
+        try:
+            ip = ip_address(raw_ip)
+        except:
+            return None
+        if 'multicast' not in whitelist:
+            if ip.is_multicast:
+                return None
+        if 'loopback' not in whitelist:
+            if ip.is_loopback:
+                return None
+        if 'link_local' not in whitelist:
+            if ip.is_link_local:
+                return None
+        if 'reserved' not in whitelist:
+            if ip.is_reserved:
+                return None
+        if 'global' not in whitelist:
+            if ip.is_global:
+                return None
+        if 'unspecified' not in whitelist:
+            if ip.is_unspecified:
+                return None
+        if 'private' not in whitelist:
+            if ip.is_private:
+                return None
+        return str(ip)
