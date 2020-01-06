ef252bd1ed3605f922ce1c9ecd64b323d3a46c95
Hunt, David <davidhunt@mitre.org>
2020-01-06 18:53:56 -0500

    rebase
    Former-commit-id: f55b8a22df901815083a6fdb8997f812adb9fa7d

--- app/obfuscators/caesar_cipher.py
+++ app/obfuscators/caesar_cipher.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from app.utility.base_obfuscator import BaseObfuscator
 
 
@@ -17,8 +19,23 @@ class Obfuscation(BaseObfuscator):
 
     def sh(self, link):
         decrypted = self.decode_bytes(link.command)
-        encrypted, shift = self.apply_cipher(decrypted)
+        encrypted, shift = self._apply_cipher(decrypted)
         link.pin = shift
         return '(shift=$(curl -s -X POST -H "Content-Type: application/json" ' \
                + self.agent.server+'/internals -d \'{"link":"%s"}\' -H "property:pin"); ' \
-                                   '$(python -c "print(\'\'.join([chr(ord(c)+-$shift) for c in \'%s\' ]))");)' % (link.unique, encrypted)
+                                   '$(python -c "print(\'\'.join([chr(ord(c)+-$shift) if c.isalnum() else c for c in \'%s\' ]))");)' % (link.unique, encrypted)
+
+    """ PRIVATE """
+
+    @staticmethod
+    def _apply_cipher(s, bounds=20):
+        """
+        Encode a command with a simple caesar cipher
+        :param s: the string to encode
+        :param bounds: the number of unicode code points to shift
+        :return: a tuple containing the encoded command and the shift value
+        """
+        shift = 0
+        while shift == 0:
+            shift = randint(-bounds, bounds)
+        return ''.join([chr(ord(c)+shift) if c.isalnum() else c for c in s]), shift
