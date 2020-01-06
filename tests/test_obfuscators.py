a155462d12bedbbfcbd900dde44c34bcd4154667
Hunt, David <davidhunt@mitre.org>
2020-01-06 18:57:23 -0500

    rebase
    Former-commit-id: 456cb5ee65c6bd8abe45eee07447460e9caf0423

--- tests/test_obfuscators.py
+++ tests/test_obfuscators.py
@@ -40,7 +40,6 @@ class TestObfuscators(TestBase):
         o = Obfuscator(name='caesar cipher', description='', module='plugins.stockpile.app.obfuscators.caesar_cipher')
         mod = o.load(self.dummy_agent)
         obfuscated_command = mod.run(self.dummy_link)
-        print(obfuscated_command)
         actual_cmd = obfuscated_command.split()[-2]
         self.assertEqual(len(self.dummy_link.command), len(actual_cmd))
 
