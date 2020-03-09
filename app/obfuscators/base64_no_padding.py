458844dabf3e454e63df7d78b939fcc67ab54e12
Ryan Frank <you@example.com>
2020-03-09 13:04:42 -0400

    add base64 no padding obfuscator and move init to base class

---
+++ app/obfuscators/base64_no_padding.py
@@ -0,0 +1,32 @@
+from base64 import b64encode
+
+from app.utility.base_obfuscator import BaseObfuscator
+
+
+class Obfuscation(BaseObfuscator):
+
+    @property
+    def supported_platforms(self):
+        return dict(
+            windows=['psh'],
+            darwin=['sh'],
+            linux=['sh']
+        )
+
+    def run(self, link, **kwargs):
+        link.command = link.command.replace('=', '')
+        return super().run(link)
+
+    def sh(self, link, **kwargs):
+        return 'eval "$(echo %s | base64 --decode)"' % str(self._add_padding(link.command).encode(), 'utf-8')
+
+    def psh(self, link, **kwargs):
+        recoded = b64encode(self.decode_bytes(self._add_padding(link.command)).encode('UTF-16LE'))
+        return 'powershell -Enc %s' % recoded.decode('utf-8')
+
+    """ PRIVATE """
+
+    def _add_padding(self, s):
+        while not self.is_base64(s):
+            s = s + '='
+        return s
