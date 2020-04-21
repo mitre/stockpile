22cab6c4d1e7f15da2e24e7350bd1ff71c6d5c28
amanners <amanners@mitre.org>
2020-04-21 15:21:51 -0400

    adding donut extension hook file

--- app/donut.py
+++ app/donut.py
@@ -7,7 +7,7 @@ async def donut_handler(services, args):
     _, file_name = await services.get('file_svc').find_file_path(args.get('file'), location='payloads')
     dir_path, donut_ext = os.path.split(file_name)
     exe_path = os.path.join(dir_path, '%s.exe' % donut_ext.split('.')[0])
-    shutil.move(src=file_name, dst=exe_path)
+    shutil.copy(src=file_name, dst=exe_path)
     shellcode = donut.create(file=exe_path)
     try:
         with open(file_name, 'wb') as f:
