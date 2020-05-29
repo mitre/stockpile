c1d3b69f1e8dead71a3785d10389e8b0e4170655
Daniel Kim <kimd@mitre.org>
2020-05-29 15:34:32 -0400

    Added timeout and fixed scanning script

--- payloads/basic_port_scanner.ps1
+++ payloads/basic_port_scanner.ps1
@@ -5,15 +5,16 @@ function PrefixLength-ToAddrInt {
 function IPv4-ToInt {
     param ([Parameter(Mandatory=$true)][string]$ipv4);
     $octets = $ipv4 -split "\." | %{ [int]$_ };
-    ($octets[0]*[Math]::Pow(256,3)) + ($octets[1]*[Math]::Pow(256,2)) + ($octets[2]*256) + $octets[3];
+    $result = ($octets[0]*[Math]::Pow(256,3)) + ($octets[1]*[Math]::Pow(256,2)) + ($octets[2]*256) + $octets[3];
+    [uint32]$result;
 };
 function Int-ToIPv4 {
     param ([Parameter(Mandatory=$true)][uint32]$ipv4int);
     $octets = @();
     $remainder = $ipv4int;
     3..0 | %{
-        $divideBy = [Math]::Pow(256, $_);
-        $octet = [int]($remainder / $divideBy);
+        $divideBy = [uint32]([Math]::Pow(256, $_));
+        $octet = [Math]::Floor($remainder / $divideBy);
         $octets += [string]$octet;
         $remainder = $remainder % $divideBy;
     };
@@ -26,27 +27,32 @@ function Get-NetAddrInt {
     );
     $maskInt = PrefixLength-ToAddrInt $prefixLength;
     $ipv4int = IPv4-ToInt $ipv4;
-    $maskInt -band $ipv4Int;
+    [uint32]($maskInt -band $ipv4Int);
 };
 function Scan-Netrange {
     param (
         [Parameter(Mandatory=$true)][string]$ipv4,
         [Parameter(Mandatory=$true)][int]$prefixLength,
-        [Parameter(Mandatory=$true)][[]int]$ports
+        [Parameter(Mandatory=$true)][int[]]$ports
     );
     $netAddrInt = Get-NetAddrInt -ipv4 $ipv4 -prefixLength $prefixLength;
-    $startAddrInt = $netAddrInt + 1;
-    $endAddrInt = $netAddrInt + [Math]::Pow(2, 32 - $prefixLength) - 1;
-    $startAddrInt..$endAddrInt | %{
-        $ipv4 = Int-ToIpv4 $_;
+    $addrCount = [int]([Math]::Pow(2, 32 - $prefixLength)) - 1; # ignore the network address
+    1..$addrCount | %{
+        $ipv4 = Int-ToIpv4 ($netAddrInt + $_);
         Write-Host "------";
         Write-Host $ipv4;
         $ports | %{
             $socket = new-object system.net.sockets.tcpclient;
-            $Connection = $socket.beginconnect($ipv4, $_, $null, $null);
-            $Connection.AsyncWaitHandle.waitOne(50,$false) | out-null;
-            if ($socket.connected -eq $true) { echo "Port $_ is open!"};
-            $socket.Close | Out-Null;
+            try {
+                $Connection = $socket.beginconnect($ipv4, $_, $null, $null);
+                if ($Connection) {
+                    $Connection.AsyncWaitHandle.waitOne(50,$false) | out-null;
+                    if ($socket.connected -eq $true) { echo "Port $_ is open!"};
+                };
+            } catch {
+            } finally {
+                $socket.Close | Out-Null;
+            };
         };
     };
 };
