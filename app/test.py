94bbaa9c87d69155c6cce6abe01209ff7e76a569
KirilldogU <kirillusubyan@gmail.com>
2022-08-05 09:43:09 -0400

    Fix: upload supplementary presentation pics

--- app/test.py
+++
@@ -1,8 +0,0 @@
-import NB_Model_Class
-import naive_bayes
-
-
-# NB_probability_obj = NB_Model_Class.NBLinkProbabilities()
-# print(NB_probability_obj.NBLinkSuccessProb({"Ability_ID": "90c2efaa-8205-480d-8bb6-61d90dbaf81b", "Link_Facts":{'file.sensitive.extension': 'wav'}, "Executor_Platform": "windows"}))
-
-# disObj = naive_bayes.LogicalPlanner(None, None)
